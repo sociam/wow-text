6b34ebb8e51688e771a229880643d058dca46ca7
electronic Max <electronic@gmail.com>
2014-11-19 18:21:45 +0000

    new squiggles with better align

--- client/js/lineplot.js
+++ client/js/lineplot.js
@@ -7,21 +7,25 @@ angular.module('bones')
 			restrict:'E',
 			scope:{ data:'=' },
 			replace:true,
-			template:'<div class="lineplot"></div>',
+			template:'<svg class="lineplot"></svg>',
 			controller:function($scope, $element, utils) {
 				// Generate a Bates distribution of 10 random variables.
 				var u = utils,
-					svg = d3.select($element[0]).append("svg"),
+					svg = d3.select($element[0]),
 					N = 40,
 					values = [];	// main data
 
-				var margin = {top: 20, right: -10, bottom: 20, left: 0},
-				    width = $element.find('svg').width(),
-				    height = $element.find('svg').height(),
+				var margin = {top: 20, right: 0, bottom: 20, left: 0},
+					width = $element.width(),
+					height = $element.height(),
 				    x = d3.scale.linear().domain([0,N]).range([margin.left,width-margin.right]),
 				    y = d3.scale.linear().range([height - margin.bottom, margin.top]),
 				    draw = function() {
 				    	if (!values.length) { return; }
+						width = $element.width();
+						height = $element.height();
+					    x = d3.scale.linear().domain([0,N]).range([margin.left,width-margin.right]);
+					    y = d3.scale.linear().range([height - margin.bottom, margin.top]);
 						var xAxis = d3.svg.axis().scale(x).orient("bottom"),
 							yAxis = d3.svg.axis().scale(y).orient("left"),
 						    line = d3.svg.line()
@@ -30,7 +34,6 @@ angular.module('bones')
 							    .y(function(d) { return y(d); });
 
 						y.domain([0, d3.max(values)]);
-						console.log('max is ', d3.max(values));
 
 						svg.selectAll('.line').data([0])
 							.enter().append("path").attr('class', 'line');
@@ -52,7 +55,11 @@ angular.module('bones')
 							if (values.length > N) { values.shift();	}
 							draw();
 						}
+						console.log('parent height ', $element.parent().height());
+						$element.height($element.parent().height());
+						$element.width($element.parent().width());						
 					});
+				console.log('lineplot component width ', width, height);
 				window.$hs = $scope;
 			}	
 		};
